40a163a3769d0464bcab0508e5925949d74de717
Stepan Lusnikov <endenwer@gmail.com>
2019-01-22 15:11:32 +0800

    Add fake data to new booking page

--- src/bali_bike/ui/screens/new_booking.cljs
+++ src/bali_bike/ui/screens/new_booking.cljs
@@ -1,8 +1,12 @@
 (ns bali-bike.ui.screens.new-booking
-  (:require [bali-bike.rn :refer [view safe-area-view scroll-view]]
-            [bali-bike.ui.components.common :refer [text button]]
+  (:require [bali-bike.rn :refer [view safe-area-view scroll-view image]]
+            [bali-bike.ui.components.common :refer [text button h3]]
+            [reagent.core :as r]
             [bali-bike.colors :as colors]
             [bali-bike.rn :as rn]
+            [bali-bike.constants :as constants]
+            [bali-bike.ui.components.bike-rating :as bike-rating]
+            [bali-bike.utils :as utils]
             [re-frame.core :as rf]))
 
 (defn render-bottom []
@@ -15,13 +19,61 @@
             :container-view-style {:margin 10}
             :text-style {:margin-horizontal 20 :font-weight "bold"}}]])
 
+(defn render-bike-title
+  [model-id]
+  [view
+   [h3 (get constants/models model-id)]])
+
+(defn render-bike-photo [photo-url]
+  [view {:style {:align-items "center" :width 100 :height 100}}
+   [image {:style {:flex 1 :background-color "skyblue" :align-self "stretch"}
+           :source {:uri photo-url}}]])
+
+(defn render-bike-preview
+  [bike-data]
+  [view {:flex-direction "row" :margin-bottom 15}
+   [render-bike-photo (first (:photos bike-data))]
+   [view {:margin-left 10}
+    [render-bike-title (:model-id bike-data)]
+    [bike-rating/main bike-data]]])
+
+(defn render-property
+  [{:keys [title value]}]
+  [view {:style {:padding-vertical 15
+                 :border-top-width 1
+                 :border-color colors/clouds}}
+   [view {:style {:flex-direction "row"
+                  :justify-content "space-between"
+                  :align-items "center"
+                  :margin-bottom 10}}
+    [h3 title]
+    [text {:style {:color colors/turquoise}} "CHANGE"]]
+   [text value]])
+
+(defn render-total []
+  [view {:style {:padding-vertical 15
+                 :border-top-width 1
+                 :border-color colors/clouds}}
+   [view {:style {:flex-direction "row"
+                  :justify-content "space-between"
+                  :align-items "center"
+                  :margin-bottom 10}}
+    [h3 "Total payment"]]
+   [text "70k * 4 days = 280k"]])
+
 (defn main []
-  [view {:style {:flex 1}}
-   [rn/map-view {:provider rn/PROVIDER_GOOGLE
-                 :style {:height 400
-                         :width 400}
-                 :initial-region {:latitude 37.78825
-                                  :longitude -122.4324
-                                  :latitudeDelta 0.0922
-                                  :longitudeDelta 0.0421}}]
-   [render-bottom]])
+  (r/with-let [current-bike (rf/subscribe [:current-bike])
+               dates-range (rf/subscribe [:dates-range])]
+    [view {:style {:flex 1}}
+     [scroll-view {:style {:flex 1
+                           :margin-horizontal 10
+                           :margin-top 10}}
+      [render-bike-preview @current-bike]
+      [render-property {:title "Dates"
+                        :value (utils/get-short-dates-range-string
+                                (:start-date @dates-range)
+                                (:end-date @dates-range))}]
+      [render-property {:title "Delivery location"
+                        :value "Aasdfjaldf, Casdffi, Bodkyfds, 123123, 348"}]
+      [render-total]]
+     [render-bottom]]))
