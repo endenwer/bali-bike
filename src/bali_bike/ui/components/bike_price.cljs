06abf8010ac16119970883c2dba46bd7eac31858
Stepan Lusnikov <endenwer@gmail.com>
2019-03-11 13:40:38 +0800

    Fix bike price component

--- src/bali_bike/ui/components/bike_price.cljs
+++ src/bali_bike/ui/components/bike_price.cljs
@@ -1,10 +1,16 @@
 (ns bali-bike.ui.components.bike-price
   (:require [re-frame.core :as rf]
+            [reagent.core :as r]
+            [bali-bike.utils :as utils]
             [bali-bike.rn :refer [view]]
             [bali-bike.ui.components.common :refer [text]]))
 
 (defn main
   [bike]
-  [view {:style {:flex-direction "row"}}
-   [text {:style {:font-weight "bold"}} (str "Rp" (:daily-price bike))]
-   [text {:style {:font-weight "500"}} "/day"]])
+  (r/with-let [dates-range (rf/subscribe [:dates-range])]
+    (let [dates-diff (utils/get-dates-diff @dates-range)
+          units (if (> (:months dates-diff) 0) "month" "day")
+          price (if (> (:months dates-diff) 0) (:monthly-price bike) (:daily-price bike))]
+      [view {:style {:flex-direction "row"}}
+       [text {:style {:font-weight "bold"}} (str "Rp " (utils/format-number price))]
+       [text {:style {:font-weight "500"}} (str " / " units)]])))
