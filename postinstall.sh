1562741fc4c9fcab5f004cc350aefc63bc8ca4b6
Stepan Lusnikov <endenwer@gmail.com>
2019-03-12 19:31:16 +0800

    Add postinstall script

--- postinstall.sh
+++ postinstall.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-brew install leiningen
-
 case $TARGET_PLATFORM in
     ios)
+        brew install leiningen
         lein do clean, with-profile prod cljsbuild once ios
         exit
         ;;
     android)
+        brew install leiningen
         lein do clean, with-profile prod cljsbuild once android
         echo
         exit
         ;;
     *)
         echo "Only ios and android target platforms are supported"
-        exit 1
+        exit
 esac
