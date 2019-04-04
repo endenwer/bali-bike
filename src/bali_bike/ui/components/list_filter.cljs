09b1d3479a2183ef0c1cf1382bc60e4cce22338f
Stepan Lusnikov <endenwer@gmail.com>
2019-04-04 09:46:21 +0800

    Fix scroll for model and area filter

--- src/bali_bike/ui/components/list_filter.cljs
+++ src/bali_bike/ui/components/list_filter.cljs
@@ -1,5 +1,5 @@
 (ns bali-bike.ui.components.list-filter
-  (:require [bali-bike.rn :refer [view search-bar list-item]]
+  (:require [bali-bike.rn :refer [view search-bar list-item scroll-view]]
             [bali-bike.utils :as utils]
             [bali-bike.colors :as colors]
             [reagent.core :as r]
@@ -9,13 +9,14 @@
   [data on-save]
   (r/with-let [value (r/atom "")]
     (let [filtered-data (utils/filter-map-by-value data @value)]
-      [view {:style {:flex-direction "column" :align-items "stretch"}}
+      [view {:style {:flex 1 :flex-direction "column" :align-items "stretch"}}
        [search-bar {:on-change-text #(reset! value %)
                     :value nil
                     :light-theme true
                     :container-style {:background-color colors/clouds
                                       :border-top-width 0}}]
-       (for [[id title] filtered-data]
-         ^{:key id} [list-item {:title title
-                                :hide-chevron true
-                                :on-press #(on-save id)}])])))
+       [scroll-view {:style {:flex 1} :showsVerticalScrollIndicator false}
+        (for [[id title] filtered-data]
+          ^{:key id} [list-item {:title title
+                                 :hide-chevron true
+                                 :on-press #(on-save id)}])]])))
